caa138340d44c6bb3a775960d4c6de61261ec67c
SirJavalot <dennis155@hotmail.com>
2015-11-02 19:48:52 +0000

    initial commit of stack

--- LiangChapter19/src/Test.java
+++ LiangChapter19/src/Test.java
@@ -1,9 +1,19 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArrayList<Double> list = new ArrayList<>();
+		list.add(5.2);
+		list.add(3.3);
+		
+		ArrayList<Date> dates = new ArrayList<>();
+		dates.add(new Date());
+		Date date = dates.get(0);
+		System.out.println(date);
+		
 	}
 
 }
